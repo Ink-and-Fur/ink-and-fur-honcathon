debe83af32fec99bafdff9e22e7019d6941aea79
Brett Beutell <brbeut@gmail.com>
2024-05-30 14:25:29 +0200

    Add mizu

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,7 +1,30 @@
 import { Hono } from 'hono'
 import { clerkMiddleware, getAuth } from '@hono/clerk-auth'
+import { Mizu, logger } from "./mizu";
 
-const app = new Hono()
+type Bindings = {
+  MIZU_ENDPOINT: string;
+};
+
+const app = new Hono<{ Bindings: Bindings }>();
+
+// Mizu Tracing Middleware - Must be called first!
+app.use(async (c, next) => {
+  const config = { MIZU_ENDPOINT: c.env.MIZU_ENDPOINT };
+  const ctx = c.executionCtx;
+
+  const teardown = Mizu.init(
+    config,
+    ctx,
+  );
+
+  await next();
+
+  teardown();
+});
+
+// Mizu request logging
+app.use(logger());
 
 app.use('*', clerkMiddleware())
 app.get('/', (c) => {
