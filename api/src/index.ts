a7982ba36e147706a9e6907f30be2a70672d5f87
Brett Beutell <brbeut@gmail.com>
2024-05-30 14:15:48 +0200

    Add clerk but not sure about how to use in react app

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,9 +1,23 @@
 import { Hono } from 'hono'
+import { clerkMiddleware, getAuth } from '@hono/clerk-auth'
 
 const app = new Hono()
 
+app.use('*', clerkMiddleware())
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
+  const auth = getAuth(c)
+
+  if (!auth?.userId) {
+    return c.json({
+      message: 'You are not logged in.',
+    })
+  }
+
+  return c.json({
+    message: 'You are logged in!',
+    userId: auth.userId,
+  })
 })
+
 
 export default app
