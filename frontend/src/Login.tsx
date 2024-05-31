69a67c1c499cbdcff9c5400378bef921f592316a
Brett Beutell <brbeut@gmail.com>
2024-05-31 15:39:15 +0200

    Add forced redirect after login to home

--- frontend/src/Login.tsx
+++ frontend/src/Login.tsx
@@ -9,8 +9,6 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
 
 export function LoginForm() {
   return (
@@ -32,7 +30,7 @@ export function LoginForm() {
         </div>
       </CardContent> */}
       <CardFooter>
-        <SignInButton>
+        <SignInButton forceRedirectUrl="/">
           <Button className="w-full">Sign in with Google</Button>
         </SignInButton>
       </CardFooter>
--- frontend/src/RootLayout.tsx
+++ frontend/src/RootLayout.tsx
@@ -1,5 +1,5 @@
 import { Outlet, useNavigate } from 'react-router-dom'
-import { ClerkProvider, SignedIn, SignedOut, UserButton } from '@clerk/clerk-react'
+import { ClerkProvider } from '@clerk/clerk-react'
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
