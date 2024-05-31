dbe14dffabc1951bd9d693fd7f973d5eb8033a67
Brett Beutell <brbeut@gmail.com>
2024-05-31 15:01:42 +0200

    Add clerk to frontend with react router

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,47 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
+import DashboardLayout, { Dashboard } from './Home.tsx'
+import { LoginForm } from './Login.tsx'
+import RootLayout from './RootLayout.tsx'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { TooltipProvider } from '@radix-ui/react-tooltip'
 
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key")
+}
+
+const router = createBrowserRouter([
+  {
+    element: <RootLayout />,
+    children: [
+      {
+        path: "/", element: <DashboardLayout />, children: [
+          { element: <Dashboard />, index: true },
+        ]
+      },
+      { path: "/login/*", element: <LoginForm /> },
+      // { path: "/sign-up/*", element: <SignUpPage /> },
+      // {
+      //   element: <DashboardLayout />,
+      //   path: "dashboard",
+      //   children: [
+      //     { path: "/dashboard", element: <DashboardPage /> },
+      //     { path: "/dashboard/invoices", element: <InvoicesPage /> }
+      //   ]
+      // }
+    ]
+  }
+])
+
+
+// biome-ignore lint/style/noNonNullAssertion: <explanation>
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <TooltipProvider>
+      <RouterProvider router={router} />
+    </TooltipProvider>
   </React.StrictMode>,
 )
