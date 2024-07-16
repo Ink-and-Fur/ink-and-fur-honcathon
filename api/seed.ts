79a0e659145eb2555006a2fa9564d70b67e3bba8
Mari <git@cutegirl.tech>
2024-07-16 15:52:47 +0200

    fix migrate and seed to work with deno

--- api/seed.ts
+++ api/seed.ts
@@ -1,24 +1,18 @@
 import { drizzle } from "drizzle-orm/neon-http";
 import { neon } from "@neondatabase/serverless";
-import { users } from "./src/db/schema";
-import { config } from "dotenv";
+import { users } from "./src/db/schema.ts";
+import { load } from "https://deno.land/std@0.224.0/dotenv/mod.ts";
 
-config({ path: '.dev.vars' });
+const env = await load();
 
 // biome-ignore lint/style/noNonNullAssertion: error from neon client is helpful enough to fix
-const sql = neon(process.env.DATABASE_URL!);
+const sql = neon(env.DATABASE_URL!);
 const db = drizzle(sql);
 
 async function seed() {
   await db.insert(users).values([
     {
-      name: "Laszlo Cravensworth",
-    },
-    {
-      name: "Nadja Antipaxos",
-    },
-    {
-      name: "Colin Robinson",
+      name: "Meowthful Whiskers",
     },
   ]);
 }
@@ -29,7 +23,7 @@ async function main() {
     console.log("Seeding completed");
   } catch (error) {
     console.error("Error during seeding:", error);
-    process.exit(1);
+    Deno.exit(1);
   }
 }
 main();
