4dcef89961d8eb3174892237cdc9e36060adf52f
Mari <git@cutegirl.tech>
2024-07-17 13:58:59 +0200

    add replicate call on create job endpoint

--- api/src/index.ts
+++ api/src/index.ts
@@ -2,6 +2,7 @@ import { clerkMiddleware, getAuth, Hono, load, neon, drizzle, eq, and } from "./
 // import { logger, Mizu } from "./mizu.ts";
 import * as AWS from "s3";
 import {jobs} from "./db/schema.ts";
+import {PREDICTION_FAILED, PREDICTION_STARTING, PREDICTION_SUCCEEDED, trainLora} from "./replicate.ts";
 
 const env = await load();
 
@@ -17,6 +18,7 @@ type Bindings = {
   DATABASE_URL: string;
   MIZU_ENDPOINT: string;
   AWS_S3_BUCKET: string;
+  BASE_URL: string,
 };
 
 const app = new Hono<{ Bindings: Bindings }>();
@@ -100,6 +102,15 @@ app.post("/api/jobs", async (c) => {
   const name = body["name"].toLowerCase(); // mby check that this is ascii only
   const file = body["file"]; // mby check that this is actually a .zip file
 
+  // prevnet the name from containing a slash as this doesnt play nicely with our callback urls using path parameters
+  if (name.includes("/")) {
+    c.status(400);
+    return c.json({
+      error: "invalid_name",
+      description: "the pet name cannot contain a slash"
+    });
+  }
+
   const result = await db.select().from(jobs).where(and(
       eq(jobs.user, userId),
       eq(jobs.name, name),
@@ -131,6 +142,25 @@ app.post("/api/jobs", async (c) => {
 
   // now that all the stuff has been successfully uploaded and inserted into our db
   // it is time to start the job
+  const callbackUrl = `${env.BASE_URL}/api/jobs/${userId}/${name}/callback`
+
+  // todo: test if this actually works
+  const { data, error } = await trainLora({
+    inputImages: url,
+    webhook: callbackUrl,
+    webhookEventsFilter: [PREDICTION_STARTING, PREDICTION_SUCCEEDED, PREDICTION_FAILED],
+  });
+
+  if (error !== null) {
+    c.status(502);
+    return c.json({
+      error: "train_lora_failed",
+      description: error.toString()
+    });
+  }
+
+  console.log(data);
+
   return c.json({
     success: true,
     job: createJobResult?.[0]
