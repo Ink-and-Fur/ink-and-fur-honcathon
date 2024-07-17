c50bd302124ffdc72af004c819e2199729b681a6
あで <adelaide@fiberplane.com>
2024-07-17 16:29:19 +0200

    Show pet details

--- frontend/src/PetDetails.tsx
+++ frontend/src/PetDetails.tsx
@@ -1,19 +1,21 @@
-import {useParams} from "react-router-dom";
+import { useParams } from "react-router-dom";
 
-import {useGetPet} from "./queries/index.ts";
+import { useGetPet } from "./queries/index.ts";
 
 export function PetDetails() {
   const { name } = useParams();
   const { data: pet, isPending } = useGetPet(name);
+  const latestUpdate = pet.updates.reverse()[0];
   return (
     <div>
-      <h1>{name}</h1>
       {isPending && <p>Loading...</p>}
       {pet && (
         <div>
-          <p>{JSON.stringify(pet, 2)}</p>
+          <h1>{pet.name}</h1>
+          <p>status: {latestUpdate.status}</p>
+          <p>updated at: {latestUpdate.created_at}</p>
         </div>
       )}
     </div>
-  )
+  );
 }
