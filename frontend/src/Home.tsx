5b61f1604debd1dfed4056d9611206bc467d2b15
Brett Beutell <brbeut@gmail.com>
2024-07-17 10:31:10 +0200

    Clean up that home layout boiiiiiiiiiiii

--- frontend/src/Home.tsx
+++ frontend/src/Home.tsx
@@ -57,14 +57,14 @@ export default function HomeLayout() {
 export function Home() {
   return (
     <div className="grid h-screen w-full pl-[56px]">
-      <aside className="inset-y fixed  left-0 z-20 flex h-full flex-col border-r">
-        <div className="border-b p-2">
+      <aside className="inset-y fixed left-0 z-20 flex h-full flex-col ">
+        <div className="py-1 px-2">
           <Button variant="outline" size="icon" aria-label="Home">
-            <Triangle className="size-5 fill-foreground" />
+            <img src="/ink-and-fur-logo-md.png" alt="Ink &amp; Fur" className="size-6" />
           </Button>
         </div>
         <nav className="grid gap-1 p-2">
-          <Tooltip>
+          {/* <Tooltip>
             <TooltipTrigger asChild>
               <Button
                 variant="ghost"
@@ -78,8 +78,8 @@ export function Home() {
             <TooltipContent side="right" sideOffset={5}>
               Pets
             </TooltipContent>
-          </Tooltip>
-          <Tooltip>
+          </Tooltip> */}
+          {/* <Tooltip>
             <TooltipTrigger asChild>
               <Button
                 variant="ghost"
@@ -108,10 +108,10 @@ export function Home() {
             <TooltipContent side="right" sideOffset={5}>
               Documentation
             </TooltipContent>
-          </Tooltip>
+          </Tooltip> */}
         </nav>
         <nav className="mt-auto grid gap-1 p-2">
-          <Tooltip>
+          {/* <Tooltip>
             <TooltipTrigger asChild>
               <Button
                 variant="ghost"
@@ -138,11 +138,11 @@ export function Home() {
           </SignedIn>
           <SignedOut>
             <Link to="/login">Log In</Link>
-          </SignedOut>
+          </SignedOut> */}
         </nav>
       </aside>
       <div className="flex flex-col">
-        <header className="sticky top-0 z-10 flex h-[57px] items-center gap-1 border-b bg-background px-4">
+        <header className="sticky top-0 z-10 flex h-[57px] items-center gap-1 bg-background px-4">
           <h1 className="text-xl font-semibold">Ink &amp; Fur</h1>
           <Button
             variant="outline"
