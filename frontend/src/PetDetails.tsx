bfb55f3490e610333346d5c42d011a95fc179410
Brett Beutell <brbeut@gmail.com>
2024-07-18 12:22:45 +0200

    Show the actual last update from the pet

--- frontend/src/PetDetails.tsx
+++ frontend/src/PetDetails.tsx
@@ -1,19 +1,23 @@
 import { useParams } from "react-router-dom";
 
 import { useGetPet } from "./queries/index.ts";
+import { useMemo } from "react";
 
 export function PetDetails() {
   const { name } = useParams();
-  const { data: pet, isPending } = useGetPet(name);
-  const latestUpdate = pet?.updates?.reverse()?.[0];
+  const { data: pet, isPending } = useGetPet(name ?? "");
+  const lastUpdateStatus = pet?.last_update;
+  const lastUpdate = useMemo(() => {
+    return pet?.updates.find((update) => update.status === lastUpdateStatus)
+  }, [pet?.updates, lastUpdateStatus]);
   return (
     <div>
       {isPending && <p>Loading...</p>}
       {pet && (
         <div>
           <h1>{pet.name}</h1>
-          <p>status: {latestUpdate.status}</p>
-          <p>updated at: {latestUpdate.created_at}</p>
+          <p>status: {lastUpdateStatus ?? "unknown"}</p>
+          <p>updated at: {lastUpdate?.completed_at || lastUpdate?.created_at}</p>
         </div>
       )}
     </div>
